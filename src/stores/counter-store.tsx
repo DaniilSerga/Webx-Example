00e6dbde08fe10de52042bfe7486c0b4f7709e33
DaniilSerga <daniil.serga@gmail.com>
2024-07-12 14:19:24 +0300

    feat: add state

--- src/stores/counter-store.tsx
+++ src/stores/counter-store.tsx
@@ -1,1 +1,34 @@
-class CounterStore {}
+// import {action, computed, makeObservable, observable} from "mobx";
+
+import {makeAutoObservable} from "mobx";
+
+class CounterStore {
+    count = 0;
+
+    get total() {
+        return this.count * 2;
+    }
+
+    constructor() {
+        // Only if needed to assign everything by hand
+        // makeObservable(this, {
+        //     count: observable,
+        //     increment: action,
+        //     decrement: action,
+        //     total: computed
+        // });
+
+        // The most common case
+        makeAutoObservable(this);
+    }
+
+    increment = (value: number) => {
+        this.count += value;
+    };
+
+    decrement = (value: number) => {
+        this.count -= value;
+    };
+}
+
+export default new CounterStore();
